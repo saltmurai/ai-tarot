2e03e13bdbcff38505ed39cc07dde13207546be1
saltmurai <phamsang6260@gmail.com>
2024-07-12 01:11:17 +0700

    fix: wrong query string

--- ai.js
+++ ai.js
@@ -19,16 +19,26 @@ export async function getGroqChatCompletion() {
 
 
 export async function getGroqTarotReading(firstCard, secondCard, thirdCard, question) {
-    return groq.chat.completions.create({
-      messages: [
-        {
-          role: "assistant",
-          content: `You are tarot reader I ask you to draw three cards for me given the ${question}. The first card is ${firstCard}, the second card is ${secondCard}, and the third card is ${thirdCard}.
-          Explain the meaning of these cards to me. and give me a summary and suggestion based on these cards. I want you to be detailed and oppinionated as much as possible. Don't speak like AI. Speak like a human tarot reader.
-          `,
-        }
-      ],
-      model: "llama3-8b-8192",
-    });
-  }
+  console.log(firstCard, secondCard, thirdCard, question);
+  
+  const content = `You are tarot reader I ask you to draw three cards for me given the ${question}. 
+  The first card is ${firstCard}, the second card is ${secondCard}, and the third card is ${thirdCard}. 
+  Explain the meaning of these cards to me. and give me a summary and suggestion based on these cards. 
+  I want you to be detailed and oppinionated as much as possible. Don't speak like AI. 
+  Speak like a human tarot reader.`;
+
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant. You reply with very short answers."
+      },
+      {
+        role: "user",
+        content: content
+      },
+    ],
+    model: "llama3-8b-8192",
+  });
+}
 
--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.get("/tarot/reading", async (req, res) => {
-    const { firstCard, secondCard, thirdCard, question } = req.query;
-    const response = await getGroqTarotReading(firstCard, secondCard, thirdCard, question);
+    const { first, second, third, question } = req.query;
+    const response = await getGroqTarotReading(first, second, third, question);
     res.send({
         status: "ok",
         response: response.choices[0].message.content,
